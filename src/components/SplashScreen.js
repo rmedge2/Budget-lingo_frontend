467569fa6d4b28a140a0e88b031dda7cb7c4886e
Rodney Edge Jr <rodneyedgejr@gmail.com>
2023-06-09 10:46:57 -0400

    added image to about page

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -1,9 +1,16 @@
 const SplashScreen = () => {
     
     return (
-        <div>
-            welcome screen
-        </div>
+        <>
+            <div className="headerRow">
+                <div className="pageTitleBox">
+                    <h2>About Us</h2>
+                </div>
+                <div className="longImage">
+                    <img src="https://c8.alamy.com/comp/M0YFXF/group-of-multi-ethnic-business-people-reading-books-against-white-M0YFXF.jpg" width={400} alt="some-img" />
+                </div>
+            </div>
+        </>
     )
 }
 export default SplashScreen
