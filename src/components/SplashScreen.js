a0a6ee468f14b74d897459195efefa0a14c6e35f
Rodney Edge Jr <rodneyedgejr@gmail.com>
2023-06-09 11:54:58 -0400

    installed bootstrap and outlined the rows further images

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -1,3 +1,4 @@
+import Image from 'react-bootstrap/Image';
 import './SplashScreen.css'
 const SplashScreen = () => {
     
@@ -7,9 +8,19 @@ const SplashScreen = () => {
                 <div className="pageTitleBox">
                     <h2>About Us</h2>
                 </div>
-                <div className="long-image">
+                {/* <div className="long-image">
                     <img src="https://c8.alamy.com/comp/M0YFXF/group-of-multi-ethnic-business-people-reading-books-against-white-M0YFXF.jpg" width={400} alt="some-img" />
-                </div>
+                </div> */}
+                <Image className='TitleBoxImg' src="https://c8.alamy.com/comp/M0YFXF/group-of-multi-ethnic-business-people-reading-books-against-white-M0YFXF.jpg" fluid />
+            </div>
+            <div className='randomInfoRow1'>
+                
+            </div>
+            <div className='randomInfoRow2'>
+                
+            </div>
+            <div className='randomInfoRow3'>
+                
             </div>
         </>
     )
