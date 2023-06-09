2a764f2016547531f12cfe2bbe0175fafbd3ab96
Conrad Quagliaroli <conradbquag@gmail.com>
2023-06-09 13:02:49 -0400

    added spacing to logs

--- src/components/Log.js
+++ src/components/Log.js
@@ -7,7 +7,7 @@ const Log = (props) => {
     const {convertDate}=useContext(BalanceData)
     
     return (
-        <div>
+        <div className='log'>
             <div className='log-name'>{props.name}</div>
             <div className='log-amount'>{props.amount}</div>
             <div className='log-date'>{convertDate(props.time)}</div>
