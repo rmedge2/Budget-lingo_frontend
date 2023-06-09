843bf8654d07b360735bc7df3b9af2e0bdb2f2a8
Rodney Edge Jr <rodneyedgejr@gmail.com>
2023-06-09 10:55:29 -0400

    just centering image on the header row

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -1,12 +1,13 @@
+import './SplashScreen.css'
 const SplashScreen = () => {
     
     return (
         <>
-            <div className="headerRow">
+            <div className="header-row">
                 <div className="pageTitleBox">
                     <h2>About Us</h2>
                 </div>
-                <div className="longImage">
+                <div className="long-image">
                     <img src="https://c8.alamy.com/comp/M0YFXF/group-of-multi-ethnic-business-people-reading-books-against-white-M0YFXF.jpg" width={400} alt="some-img" />
                 </div>
             </div>
