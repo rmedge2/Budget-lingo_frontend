5a03d249ba4cc43a6890d64b6bca65ecf93e2f10
Conrad Quagliaroli <conradbquag@gmail.com>
2023-06-09 14:54:04 -0400

    added edit item

--- src/components/Log.js
+++ src/components/Log.js
@@ -15,16 +15,19 @@ const Log = (props) => {
     }
     
     return (
-        <div className='log'>
-            <div className='name-amount'>
-                <h3 className='log-name'>{props.name}</h3>
-                <h6 className='log-amount' style={{ color: determineColor(props.amount) }}>${props.amount}</h6>
+        <div className='log-box'>
+            <div className='log'>
+                <div className='name-amount'>
+                    <h3 className='log-name'>{props.name}</h3>
+                    <h6 className='log-amount' style={{ color: determineColor(props.amount) }}>${props.amount}</h6>
+                </div>
+                <div className='date-total'>
+                    <h5 className='log-date'>{convertDate(props.time)}</h5>
+                    <h6 className='log-total'>total</h6>
+                </div>
             </div>
-            <div className='date-total'>
-                <h5 className='log-date'>{convertDate(props.time)}</h5>
-                <h6 className='log-total'>total</h6>
-            </div>
-    
+            {/* <button className='edit'>edit</button> */}
+            <img className='edit-icon' src={require('../Edit_icon_(the_Noun_Project_30184).svg.png')} alt="edit" />
         </div>
     )
 }
