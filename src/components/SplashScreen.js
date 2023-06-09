1c2734379676b05fba7c46838e2891e4e7911eb7
Rodney Edge Jr <rodneyedgejr@gmail.com>
2023-06-09 12:42:02 -0400

    added more outline to splashscreen page

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -1,7 +1,9 @@
 import Image from 'react-bootstrap/Image';
+import Card from 'react-bootstrap/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './SplashScreen.css'
 const SplashScreen = () => {
-    
+
     return (
         <>
             <div className="header-row">
@@ -13,14 +15,47 @@ const SplashScreen = () => {
                 </div> */}
                 <Image className='TitleBoxImg' src="https://c8.alamy.com/comp/M0YFXF/group-of-multi-ethnic-business-people-reading-books-against-white-M0YFXF.jpg" fluid />
             </div>
-            <div className='randomInfoRow1'>
-                
+            <div className='randomInfoRow'>
+                <div className='rowText'>
+                    <Card style={{ width: '18rem', height: '11rem' }}>
+                        <Card.Body>
+                            <Card.Title>Easily Manage Personal Finances</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+
             </div>
-            <div className='randomInfoRow2'>
-                
+            <div className='randomInfoRow'>
+                <div className='rowText'>
+                    <Card style={{ width: '18rem', height: '11rem' }}>
+                        <Card.Body>
+                            <Card.Title>A Fun Way to Manage Finances</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                            <Card.Text>
+                                Some fake critic
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+
+                </div>
             </div>
-            <div className='randomInfoRow3'>
-                
+            <div className='randomInfoRow'>
+                <div className='rowText'>
+                    <Card style={{ width: '18rem', height: '11rem' }}>
+                        <Card.Body>
+                            <Card.Title>Revolutionize your finance today</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                            <Card.Text>
+                                Some fake critic
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
             </div>
         </>
     )
