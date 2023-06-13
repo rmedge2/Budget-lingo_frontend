b7a28e04631ae60b40603859a1561b9b59242764
Rodney Edge Jr <rodneyedgejr@gmail.com>
2023-06-13 09:35:46 -0400

    small insignificant change

--- src/components/LogInPage.js
+++ src/components/LogInPage.js
@@ -41,8 +41,8 @@ const LoginPage = () => {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json'
                 },
-                body: jsonData
-            }
+                body: jsonData,
+            },
         )
         setCurrUser(currentUser)   
         setErr('registered!')
